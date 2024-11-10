ed3056bd8647568b2e9271f0e11f609b41316132
1dixon1 <158303177+1dixon1@users.noreply.github.com>
2024-11-10 12:36:47 +0200

    Update components.jsx

--- src/components/script/components.jsx
+++ src/components/script/components.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Підключення CSS
 import '../style/components.css';
@@ -12,8 +12,6 @@ export function Button({className, children, onClick }) {
         <button className = { className} onClick={onClick} >{children} </button>
     );
 }
-
-
 
 
 export default Components;
